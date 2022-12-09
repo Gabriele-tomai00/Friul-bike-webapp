9e04e99f24fcaa2a6dd2cbcf4a621f5139c14f3b
Gabriele <tomaigabriele@gmail.com>
2022-12-09 20:24:55 +0100

    feat: add databasehandler file e script su server.js

---
+++ DataBaseHandler.js
@@ -0,0 +1,54 @@
+module.exports = class DataBaseHandler {
+    constructor(fileName) {
+        this.fileName = fileName;
+        this.fs = require('fs');
+        this.db = [];
+
+        this.loadDB();
+    }
+
+    checkForFile(callback)
+    {
+        this.fs.exists(this.fileName, function (exists) {
+            if(exists)
+            {
+                callback();
+            }else
+            {
+                fs.writeFile(this.fileName, '[]', function (err, data) 
+                { 
+                    callback();
+                })
+            }
+        });
+    }
+
+    add(entry) {
+        this.db.push(entry);
+        this.saveToStorage();
+    }
+
+    loadDB(){
+        try {
+            if (this.fs.existsSync(this.fileName)) {
+                this.db = JSON.parse(this.fs.readFileSync(this.fileName, 'utf8'));
+            } else {
+                this.saveToStorage();
+                console.log("Save empyy");
+            }
+          } catch(err) {
+            console.error(err)
+            this.saveToStorage();
+          }
+    }
+
+    saveToStorage(){
+        let toSave = JSON.stringify(this.db);
+        console.log(toSave);
+        this.fs.writeFile(this.fileName, toSave, 'utf8', function (err, data) 
+        { 
+            console.log(data, err);
+        })
+    }
+}
+
--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 app.use(express.static('/public'))
 
+const DataBaseHandler = require('./DataBaseHandler');
+
 
 app.use(express.static('public')); //per hostare tutto quello che è nella cartella public
+
+let dbh = new DataBaseHandler("comments.dat");
+app.post('/api/comments/add/mtb/m1', function(req, res) {
+    dbh.add(req.body);
+    res.send("ok");
+});
+app.get('/api/comments/list/mtb/m1', function(req, res) {
+    res.send(JSON.stringify(dbh.db));
+});
+
+
 
 app.listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/Home.html');
