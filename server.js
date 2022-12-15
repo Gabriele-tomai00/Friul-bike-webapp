46285717dfea9768082b756129c3588f7835f4fc
Gabriele <tomaigabriele@gmail.com>
2022-12-15 10:56:54 +0100

    feat: nodeserver stampa l'url come m1.html per comodità

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.get('/api/comments/list/mtb/m1', function (req, res) {
 
 // metto in ascolto il server all'indirizzo 127.0.0.1
 app.listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/Home.html');
+//console.log('Server running at http://127.0.0.1:1337/Home.html');
+console.log('Server running at http://127.0.0.1:1337/paths/mtb/m1.html');
