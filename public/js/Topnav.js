aa9d582493d8ea16e9b2691b845502caf858eec6
Gabriele <tomaigabriele@gmail.com>
2022-12-22 11:02:52 +0100

    fix: js rimpiazzato con Scripts (percorso cartella)

--- public/js/Topnav.js
+++
@@ -1,9 +0,0 @@
-function show_links() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
