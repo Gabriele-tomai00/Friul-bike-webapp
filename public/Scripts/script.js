79f77c7e137a93123b86ace4ccf8020763028af0
Gabriele <tomaigabriele@gmail.com>
2023-01-09 21:49:34 +0100

    fix: corretta funzione per visualizzare e nascondere la descrizione

--- public/Scripts/script.js
+++ public/Scripts/script.js
@@ -7,14 +7,35 @@ function showLinks() {
     }
 }
 
+let firstTime = true;
 function loadDescription(name) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("desc").innerHTML =
-                this.responseText;
+    let descElem = document.getElementById("desc");
+
+    if (firstTime) {
+        console.log("Primo caricamento da file");
+
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("desc").innerHTML =
+                    this.responseText;
+            }
+        };
+        xhttp.open("GET", "../../../Contents/Descriptions/" + name + ".txt", true);
+        xhttp.send();
+
+        firstTime = false;
+    }
+    else
+    {
+        if (descElem.style.display === "none") {
+            descElem.style.display = "block";
+            document.getElementById("loadDesc").innerHTML = "Nascondi descrizione";
         }
-    };
-    xhttp.open("GET", "../../../Contents/Descriptions/" + name + ".txt", true);
-    xhttp.send();
+        else
+        {
+            descElem.style.display = "none";
+            document.getElementById("loadDesc").innerHTML = "Visualizza descrizione";
+        }
+    }
 }
