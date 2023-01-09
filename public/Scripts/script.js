16f77334a7d3ca927d3acd2554b1f06c58e39f1c
Gabriele <tomaigabriele@gmail.com>
2023-01-09 20:33:53 +0100

    feat: aggiunta xmlhttprequest di un file txt contenente la descrizione del percorso

--- public/Scripts/script.js
+++ public/Scripts/script.js
@@ -7,3 +7,14 @@ function showLinks() {
     }
 }
 
+function loadDescription(name) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("desc").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "../../../Contents/Descriptions/" + name + ".txt", true);
+    xhttp.send();
+}
